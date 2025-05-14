de5b317344e19262d59078626c7cea10804efafb
Michael Jovanco <michaeljovanco_2327240023@mhs.mdp.ac.id>
2025-05-14 13:55:31 +0700

    push blade

---
+++ resources/views/fakultas/create.blade.php
@@ -0,0 +1,39 @@
+@extends('layout.main')
+@section('title', 'Fakultas')
+@section('content')
+    <!--begin::Row-->
+    <div class="row">
+        <div class="col-12">
+          <!-- Default box -->
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">List Falkutas</h3>
+              <div class="card-tools">
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-collapse"
+                  title="Collapse"
+                >
+                  <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                  <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-tool"
+                  data-lte-toggle="card-remove"
+                  title="Remove"
+                >
+                  <i class="bi bi-x-lg"></i>
+                </button>
+              </div>
+            </div>
+            <div class="card-body">
+                {{-- Form tambah gakultas --}}
+            </div>
+            <!-- /.card-body -->
+          <!-- /.card -->
+        </div>
+      </div>
+      <!--end::Row-->
+      @endsection
